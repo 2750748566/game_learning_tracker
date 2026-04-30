8c27d82ad17dc52a48107d06d1bbc46bd4cccdfa
2750748566 <2750748566@qq.com>
2026-05-01 00:21:44 +0800

    Initialize task_system as a Python package

---
+++ task_system/init.py
@@ -0,0 +1,6 @@
+# 这个文件标识 task_system 是一个 Python 包
+# 可以为空，或者添加一些导入语句
+
+from .task_system import GameLearningSystem, TaskType, TaskStatus
+
+__all__ = ['GameLearningSystem', 'TaskType', 'TaskStatus']
